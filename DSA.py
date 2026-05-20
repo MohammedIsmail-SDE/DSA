daf0527ccddb00db67f46449db581f5fc883d006
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-05-20 22:53:52 +0530

    dsa

--- DSA.py
+++ DSA.py
@@ -24,6 +24,13 @@
 
 from typing import List
 
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+
 class Solution:
     def getImportance(self, employees: List['Employee'], id: int) -> int:
         # step one -- prepare adjacency list 
