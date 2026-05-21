3919fbd1c69230f7b1de271eb73ba64d6a06dbf9
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-05-21 15:30:18 +0530

    dsa sheet

--- 1DSA.py
+++ 1DSA.py
@@ -1,1 +1,6 @@
-wir
+# 1Q.write a program that will convert celsius value to fahrenheit 
+cles_fahren = int (input("enter celsius:"))
+
+fahren_temp = 1.8*(cles_fahren)
+result = 32+fahren_temp
+print("fahren_temp", result)
