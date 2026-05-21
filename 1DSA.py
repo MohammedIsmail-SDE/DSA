a7de545cb3f6c9c1e31c89102665eff49571f13c
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-05-21 15:38:12 +0530

    dsa sheet

--- 1DSA.py
+++ 1DSA.py
@@ -1,6 +1,7 @@
 # 1Q.write a program that will convert celsius value to fahrenheit 
-cles_fahren = int (input("enter celsius:"))
+cell_faharn = int(input("enter clesius :"))
 
-fahren_temp = 1.8*(cles_fahren)
-result = 32+fahren_temp
-print("fahren_temp", result)
+faharn_temp = 1.8*(cell_faharn)
+result = 32+faharn_temp
+
+print("faharn_temp :-",result)
--- DSA.py
+++ DSA.py
@@ -52,3 +52,8 @@ class Solution:
         return recur(adjList[id])
 
 
+cles_fahren = int (input("enter celsius:"))
+
+fahren_temp = 1.8*(cles_fahren)
+result = 32+fahren_temp
+print("fahren_temp", result)
