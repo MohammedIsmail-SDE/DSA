483c6c8d1df4552254f60c186244ce3dab506ed2
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-05-26 16:10:35 +0530

    dsa

--- 1DSA.py
+++ 1DSA.py
@@ -24,4 +24,29 @@
 # print("enter number is ",number)
 class Solution:
     def mergeArrays(self, nums1, nums2):
-        print
+        """Merge two lists of [id, value] by id, summing values for equal ids and
+        returning a sorted list by id.
+        """
+        i, j = 0, 0
+        res = []
+        while i < len(nums1) and j < len(nums2):
+            id1, val1 = nums1[i]
+            id2, val2 = nums2[j]
+            if id1 == id2:
+                res.append([id1, val1 + val2])
+                i += 1
+                j += 1
+            elif id1 < id2:
+                res.append([id1, val1])
+                i += 1
+            else:
+                res.append([id2, val2])
+                j += 1
+        # append remaining
+        while i < len(nums1):
+            res.append(nums1[i])
+            i += 1
+        while j < len(nums2):
+            res.append(nums2[j])
+            j += 1
+        return res
