ec136f69939bec375991187a2bc6f17aabbdba34
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-02 12:15:27 +0530

    dsa

--- 1DSA.py
+++ 1DSA.py
@@ -52,6 +52,10 @@
 #         return res
 # import pyttsx3
 # pyttsx3.speak("h")
-weight_lbs = input("please enter weight in pounds : ")
-weight_kg = int(weight_lbs)*0.45
-print ("kg is ",weight_kg)
+# weight_lbs = input("please enter weight in pounds : ")
+# weight_kg = int(weight_lbs)*0.45
+# print ("kg is ",weight_kg)
+frist = input("enter your frist name :")
+last = input ("enter your last name :")
+full_name = f'{frist} [{last}] is a coder'
+print(full_name)
