bccd621b3f6c3678a695379fce397dec02a6f7d9
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-02 11:49:09 +0530

    dsa

--- 1DSA.py
+++ 1DSA.py
@@ -52,9 +52,7 @@
 #         return res
 # import pyttsx3
 # pyttsx3.speak("h")
-user = int(input("please enter weight in pounds : "))
-kg = user
-float = 0.453
-fun = (user*float)
+weight_lbs = input("please enter weight in pounds : ")
+weight_kg = int(weight_lbs)*0.45
 
-print ("kg is ",fun)
+print ("kg is ",weight_kg)
