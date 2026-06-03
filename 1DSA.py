8cb6c82752cdffd29f96eda26650e6f4c8c18d30
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-03 17:53:47 +0530

    dsa

--- 1DSA.py
+++ 1DSA.py
@@ -107,15 +107,18 @@
 #     print ("name cannot be more than 50 characters")
 # else :
 #     print("Hi",name)
-num = int (input ( "please enter weight : "))
-units = input("This weight is in (L)bs or (k)g :")
+# num = int (input ( "please enter weight : "))
+# units = input("This weight is in (L)bs or (k)g :")
 
-if units.upper() == "L":
-    converted = round(num *0.45,3)
+# if units.upper() == "L":
+#     converted = round(num *0.45,3)
 
-    print(f'you are {converted} kilo')
-else:
-    converted = round(num / 0.45,3)
-    print(f"your are {converted} pounds")
+#     print(f'you are {converted} kilo')
+# else:
+#     converted = round(num / 0.45,3)
+#     print(f"your are {converted} pounds")
 
-
+i = 1
+while i <=5:
+ print(i)
+ i = i+1
