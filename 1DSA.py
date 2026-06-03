bd8d8caca62beeb61014c925a73210c03addf3db
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-03 11:54:11 +0530

    dsa

--- 1DSA.py
+++ 1DSA.py
@@ -99,23 +99,23 @@
 # else :
 #     print ("its a normal day")
 
-# name = input("please enter your name :")
+name = input("please enter your name :")
 
-# if len(name )<3:
-#     print ("name must be above than 3 characters")
-# elif len(name)>50 :
-#     print ("name cannot be more than 50 characters")
-# else :
-#     print("Hi",name)
-num = int (input ( "please enter weight : "))
-units = input("this weight is in (L)bs or (k)g :")
+if len(name )<3:
+    print ("name must be above than 3 characters")
+elif len(name)>50 :
+    print ("name cannot be more than 50 characters")
+else :
+    print("Hi",name)
+# num = int (input ( "please enter weight : "))
+# units = input("this weight is in (L)bs or (k)g :")
 
-if units.upper() == "L":
-    converted = round(num *0.45,3)
+# if units.upper() == "L":
+#     converted = round(num *0.45,3)
 
-    print(f'you are {converted} kilo')
-else:
-    converted = round(num / 0.45,3)
-    print(f"your are {converted} pounds")
+#     print(f'you are {converted} kilo')
+# else:
+#     converted = round(num / 0.45,3)
+#     print(f"your are {converted} pounds")
 
 
