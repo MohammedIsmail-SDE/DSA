4a5877e81b91ccb34e7f3ad17b2c436e525936c8
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-04 17:54:15 +0530

    dsa

--- DSA practice.py
+++ DSA practice.py
@@ -2,15 +2,15 @@ start = "car is started"
 stop = "car is stopped"
 
 while True:
-    game = input(">").lower()
-    if game == "start":
-        print(start)
-    elif game == "stop":
-        print(stop)
-    elif game == "help":
-        print("start - to start a car\nstop - to stop the car\nquit - to quit")
-    elif game == "quit":
-        print("Game is over")
-        break
-else:
- print("I don't understand 🤷‍♂️")
+        game = input("> ").lower()
+        if game == "start":
+                print(start)
+        elif game == "stop":
+                print(stop)
+        elif game == "help":
+                print("start - to start a car\nstop - to stop the car\nquit - to quit")
+        elif game == "quit":
+                print("Game is over")
+                break
+        else:
+                print("I don't understand 🤷‍♂️")
