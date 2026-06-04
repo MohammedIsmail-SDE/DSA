6eaf18154451120e818cf7dec75651c76052d626
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-04 16:16:18 +0530

    dsa

--- DSA practice.py
+++ DSA practice.py
@@ -1,4 +1,21 @@
+start = " car is ready to go "
+stop = "car is stopped"
+game=input (">").lower
 
+while game =="start":
+    print(start)
+    break
+if game == "stop":
+     print(stop)
+
+if game == "help ":
+      print ("start - to start a car  ")
+
+if game == "quit" :
+    print ("Game is over")
+    
+else :
+  print("i dont understand 🤷‍♂️")
 
 
 
