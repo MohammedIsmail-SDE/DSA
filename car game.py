f98eb27e4ecef6a01128a1cc66f0f39fb43ccbb0
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-04 19:41:13 +0530

    dsa

--- car game.py
+++ car game.py
@@ -1,14 +1,44 @@
-stop = "car stopped"
-start = " Car Started...Ready To GO!"
+# stop = "car stopped"
+# start = " Car Started...Ready To GO!"
 
-while 1:
-     car_game = input(" >").lower()
-     if car_game == "start" :
-          print (start)
-     elif car_game == "help":
-          print(" start - To start the car \n stop - to stop the car \n quit - to exit \n")
-     elif car_game in {"stop", "quit"} :
-         if car_game == "stop": print(stop)
-         break
-     else:
-          print ("I don't understand that...🤷‍♂️")
+# while 1:
+#      car_game = input(" >").lower()
+#      if car_game == "start" :
+#           print (start)
+#      elif car_game == "help":
+#           print(" start - To start the car \n stop - to stop the car \n quit - to exit \n")
+#      elif car_game in {"stop", "quit"} :
+#          if car_game == "stop": print(stop)
+#          break
+#      else:
+#           print ("I don't understand that...🤷‍♂️")
+
+start = "car is started"
+stop = "car is stopped"
+is_car_started = False
+is_car_stopped = False
+while True:
+        game = input("> ").lower()
+        # breakpoint()
+        if game == "start":
+                if not is_car_started:
+                    print(start)
+                    is_car_started = True
+                else:
+                    print("Car Already running.....")
+        elif game =="start" :
+                print("Car is already started")
+        elif game == "stop":
+                if not is_car_stopped :
+                 print(stop)
+                 is_car_stopped = True
+                else :
+                       print("car is already stopped !!!!")
+
+        elif game == "help":
+                print("start - to start a car\n stop - to stop the car\n quit - to quit")
+        elif game == "quit":
+                print("Game is over")
+                break
+        else:
+                print("I don't understand 🤷‍♂️")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,1 @@
-number = 9
-staring_number = 0
-ending_number = 3
-while staring_number < ending_number :
-     game = int(input("Guess :"))
-     staring_number += 1
-     if game == number:
-          print ("You win")
-          break
-else:
-     print ("Sorry you lost")
-wining_number = 9 
-staring_number = 0
-ending_number = 4
+        # breakpoint()
