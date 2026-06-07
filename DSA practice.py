dff400e0532ce0b266e2aa6afb580f03afecc84f
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-07 12:35:55 +0530

    dsa

--- DSA practice.py
+++ DSA practice.py
@@ -1,7 +1,6 @@
-num_1=[1,1,1,24,8]
-num = num_1 [0]
-for number in num_1:
-    if number > num :
-        num = number
-    
-print(num)
+num_1=[98,66,102,65]
+num_2 = num_1[0]
+for number in num_1 :
+    num_2 > number 
+print(num_2)
+
