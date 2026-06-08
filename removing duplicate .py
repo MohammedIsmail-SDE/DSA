7c5a152ee2f99984981513416e838b0a12f2335d
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-08 16:59:58 +0530

    dsa

---
+++ removing duplicate .py
@@ -0,0 +1,13 @@
+# num_1 = [1,2,3,3,4,4,5,5,9,2]
+# num_2= []
+# for number in num_1:
+#     if number not in num_2:
+#         num_2.append(number)
+# print(num_2)
+
+num = [1,2,3,1,2,3]
+num2 = []
+for number in num :
+    if number not in num2 :
+        num2.append(number)
+print(num2)
