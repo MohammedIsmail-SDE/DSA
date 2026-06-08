3d154aa4442911b4188c971bf8fc486546470e36
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-08 13:18:42 +0530

    dsa

---
+++ 2D list .py
@@ -0,0 +1,16 @@
+# lists = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+# lists [0][2] = 20
+# print(lists[0][2])
+
+lists = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+for row in lists :
+    for iteam in row :
+        print(iteam)
