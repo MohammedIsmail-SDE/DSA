bf38b691e06fc230943813d87a1d3da751c5afb3
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-09 15:51:58 +0530

    dsa

--- DSA practice.py
+++ DSA practice.py
@@ -1,5 +1,10 @@
 massage = input(">")
+words = massage.split(' ')
 emojie = {
-    ":)":
-    
+    ":)": "😊",
+    "):":"😥",
 }
+output =" "
+for word in words :
+    output += emojie.get(word,word) + " "
+print(output)
