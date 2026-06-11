ce2e7982be12c57ba3ee1fdd7602590fa9cbd010
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-11 21:46:03 +0530

    classes

--- DSA practice.py
+++ DSA practice.py
@@ -1,15 +1,1 @@
 
-def enmoji_Username():
-    words = massage.split(' ')
-    emojis = {
-    ":)": "😊",
-    ":(": "😢"
-            }    
-    output = ""
-    for word in words:
-        output += emojis.get(word, word) + " "
-        return output
-        
-        
-massage = input(">")
-print(enmoji_Username(massage))  
--- function.py
+++ function.py
@@ -1,6 +1,22 @@
-def user_name(fristname,secondname):
-    print (f'Hi {fristname} {secondname}!')
+# def user_name(fristname,secondname):
+#     print (f'Hi {fristname} {secondname}!')
     
-print("start")
-user_name("Mohammed","ismail")
-print("😊")
+# print("start")
+# user_name("Mohammed","ismail")
+# print("😊")
+
+
+def enmoji_Username():
+        words = massage.split(' ')
+        emojis = {
+        ":)": "😊",
+        ":(": "😢"
+                }    
+        output = ""
+        for word in words:
+                output += emojis.get(word, word) + " "
+        return output
+        
+        
+massage = input(">")
+print(enmoji_Username(massage)) 
