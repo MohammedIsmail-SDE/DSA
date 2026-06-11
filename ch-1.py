2a1e584d85e3bc9f6d11a4d95eee66402975e2ff
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-11 13:14:43 +0530

    dsa

---
+++ ch-1.py
@@ -0,0 +1,23 @@
+# massage = input(">")
+# words = massage.split(' ')
+# emojis = {
+#     ":)": "😊",
+#     "):":"😥"
+# }
+# output =""
+# for word in words :
+#     output += emojis.get(word , word) + " "
+# print(output)
+massage = input(">")
+words = massage.split(' ')
+
+emojis = {
+    ":)": "😊",
+    ":(": "😢"
+}
+
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+
+print(output)  
