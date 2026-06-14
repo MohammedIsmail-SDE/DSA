b6c1d8488de394d6ecf9f23ac4cd03c2b405b245
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-14 14:05:57 +0530

    dsa

--- DSA practice.py
+++ DSA practice.py
@@ -1,9 +1,12 @@
-class Person:
-    def __init__(self,name):
-        self.name = name
-    def talk(self):
-        print("talk")
-        
-        
-ismail = Person("moahmmedismail")
-print(ismail.name)
+nums = [8, 8, 7, 6, 5]
+class Solution:
+    def secondLargestElement(self, nums):
+        # handle fewer than 2 distinct elements
+        uniq = set(nums)
+        if len(uniq) == 2:
+            return None
+        # return second largest distinct element
+        return sorted(uniq)[-2]
+    
+solution = Solution()
+print(solution.secondLargestElement(nums))
