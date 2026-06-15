6f88ea145f90b62f676e4c303cc03fc4d370ebe0
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-15 12:52:28 +0530

    dsa

--- DSA practice.py
+++ DSA practice.py
@@ -1,4 +1,15 @@
-import Second_Largest_Element 
-
-Second_Largest_Element.Solution
-
+def max_number(number):
+    
+    if not number :
+        return None
+    
+    max_value = number(0)
+    
+    for nume in number :
+        if nume >max_value :
+            max_value = nume 
+            return max_value
+        
+        
+my_list= [1,2,5,8,99,66]
+print(max_number(my_list))
