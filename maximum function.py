c3a28162b6b4e3ea06708ec53eeb2ebc3ceacb8f
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-15 13:07:53 +0530

    Max function

---
+++ maximum function.py
@@ -0,0 +1,13 @@
+def max_number(number):
+    
+    if not number :
+        return None
+    
+    max_value = number [0]
+    
+    for nume in number :
+        if nume > max_value :
+            max_value = nume 
+    return max_value
+        
+        
