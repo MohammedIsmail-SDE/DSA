6ab399b329ab4260e699986902bb5a02d4aa0176
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-15 13:02:12 +0530

    dsa

--- DSA practice.py
+++ DSA practice.py
@@ -3,12 +3,12 @@ def max_number(number):
     if not number :
         return None
     
-    max_value = number(0)
+    max_value = number [0]
     
     for nume in number :
         if nume >max_value :
             max_value = nume 
-            return max_value
+        return max_value
         
         
 my_list= [1,2,5,8,99,66]
