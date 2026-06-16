fdb2f7f7695a27a8082eb760a134d0ec451dafb0
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-16 12:40:56 +0530

    dsa

---
+++ DSA practice.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeDuplicates(input):
+        sorted_array =list(dict.fromkeys(input))
+        return list(dict.fromkeys(input))
+    
+    
+nums = [1,1,2]
+print(Solution.removeDuplicates(nums))
+
+#    while k:
+#             temp = nums.pop()
+#             nums.insert(0, temp)
+#             k -= 1
