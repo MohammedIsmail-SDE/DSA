24bbab6181f272ec2dc5229d5fcc74eb4ed013f0
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-17 16:16:54 +0530

    dsa

--- DSA practice.py
+++ DSA practice.py
@@ -1,29 +1,22 @@
-# import openpyxl as xl
-# file = xl.load_workbook('STOCK_18%(1).xlsx')
-# sheet = file['Stock Summary']
-
-# for row in range(4,sheet.max_row+1):
-#     cell_1=sheet.cell(row,6)
-#     cell_2=sheet.cell(row,7)
-    
-#     value_1 = cell_1.value if cell_1.value is not None else 0
-#     value_2 = cell_2.value if cell_2.value is not None else 0
-#     total_price = value_1 + value_2
-    
-#     sheet.cell(row,8).value=total_price
-    
-    
-    
-# file.save('STOCK_18%(1).xlsx')
     
 import openpyxl as xl
 
-# Load the workbook and select the sheet
-file = xl.load_workbook('STOCK_18%(1).xlsx')
-sheet = file['Stock Summary']
+from openpyxl.chart import BarChart,Reference
 
-# Loop through rows starting from row 4
-for row in range(3, sheet.max_row + 1):
-    sheet.cell(row,4).value = f"=B{row}*C{row}"
+def automate_file(filename):
+    
+    file = xl.load_workbook(filename)
+    sheet = file['Stock Summary']
 
-file.save('STOCK_18%(1).xlsx')
+    for row in range(3, sheet.max_row + 1):
+        sheet.cell(row,4).value = f"=B{row}*C{row}"
+        
+    value = Reference(sheet,min_row=4,max_row=sheet.max_row,min_col=4,max_col=4)
+    chart = BarChart()
+    chart.add_data(value)
+    sheet.add_chart(chart,'e3')
+
+    file.save(filename)
+    
+    
+automate_file = ('STOCK_18%(1).xlsx')
