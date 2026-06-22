0c00249634230d0c2c16f47b23b4a364c642f056
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-22 15:37:40 +0530

    dsa

--- DSA practice.py
+++ DSA practice.py
@@ -1,4 +1,10 @@
-nums = [1,2,3,4,5,6,7,8]
-k = 3
-value = nums[-k:] + nums[:-k]
-print(value)
+class Solution:
+    def moveZeroes(self, nums: list[int]) -> None:
+        left = 0
+        
+        # Iterate through the array with a right pointer
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                # Swap the non-zero element with the element at the left pointer
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
