63a63f1b2eeac8aceccd792912942f4922158d65
MohammedIsmail-SDE <mohammedismail3220@gmail.com>
2026-06-26 15:19:32 +0530

    dsa

--- DSA practice.py
+++ DSA practice.py
@@ -1,8 +1,16 @@
+nums1 = [1, 2, 3, 4, 5]
+nums2 = [1, 2, 7]
 
-nums = [2, 3, 4, 5, 3]
-target = 3
+nums3 = nums1 + nums2 
+result = (set(nums3))
 
-for i in range(len(nums)):
-    if nums[i] == target :
-        break
-    
+print(nums3)
+
+
+# list_a = [1, 2, 3, 4, 5]
+# list_b = [3, 4, 5, 6, 7]
+
+# merged = list_a + list_b
+# result = list(set(merged))
+
+# print(result)
